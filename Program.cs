517ae2ca834d2f065e69b18e45fdf4f304f851d7
williamJmoriarty <140976914+ilhamlii21@users.noreply.github.com>
2025-03-11 12:22:02 +0700

    Update : Menambahkan Fitur GUI

--- Program.cs
+++ Program.cs
@@ -1,23 +1,52 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using tpmoodul3_2311104068;
 
-namespace anjay
+namespace GUIExample
 {
-    static class Program
+    public class MainForm : Form
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
+        private TextBox inputBox;
+        private Button submitButton;
+        private Label outputLabel;
+
+        public MainForm()
+        {
+            this.Text = "GUI TP MODUL 3";
+            this.Size = new System.Drawing.Size(400, 200);
+            this.BackColor = System.Drawing.Color.White;
+
+            inputBox = new TextBox();
+            inputBox.Location = new System.Drawing.Point(50, 30);
+            inputBox.Size = new System.Drawing.Size(200, 30);
+            this.Controls.Add(inputBox);
+
+            submitButton = new Button();
+            submitButton.Text = "Submit";
+            submitButton.Location = new System.Drawing.Point(260, 30);
+            submitButton.Click += new EventHandler(SubmitButton_Click);
+            this.Controls.Add(submitButton);
+
+            outputLabel = new Label();
+            outputLabel.Text = "Output";
+            outputLabel.Location = new System.Drawing.Point(50, 80);
+            outputLabel.Size = new System.Drawing.Size(300, 30);
+            outputLabel.BackColor = System.Drawing.Color.AliceBlue;
+            outputLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.Controls.Add(outputLabel);
+        }
+
+        private void SubmitButton_Click(object sender, EventArgs e)
+        {
+            string inputText = inputBox.Text;
+            outputLabel.Text = "Halo Selamat datang hoy " + inputText;
+        }
+
         [STAThread]
-        static void Main()
+        public static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
